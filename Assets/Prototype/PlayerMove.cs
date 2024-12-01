59844640b7d9cf0ffdca695a0582128c68fb6a09
Ast <93723066+YaboiAst@users.noreply.github.com>
2024-11-30 22:27:47 -0300

    Animations on Edson

--- Assets/Prototype/PlayerMove.cs
+++ Assets/Prototype/PlayerMove.cs
@@ -8,11 +8,12 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rb;
-    // private Tween moveTween;
-
+    private Animator animator;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private Vector2 direction;
@@ -29,12 +30,20 @@ public class PlayerMove : MonoBehaviour
             direction = movementVector.y > 0 ? Vector2.up : Vector2.down;
 
         if (rb.velocity != Vector2.zero) return;
+        if (direction == -1 * ((Vector2) transform.up)) return;
+        
+        animator.SetTrigger("Dash");
+
+        transform.right = direction;
         rb.AddForce(direction * (speed * Time.deltaTime), ForceMode2D.Impulse);
     }
+
+    private void BackToIdle() => animator.SetTrigger("Landed");
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         transform.up = -direction;
         rb.velocity = Vector2.zero;
+        animator.SetTrigger("Landed");
     }
 }
